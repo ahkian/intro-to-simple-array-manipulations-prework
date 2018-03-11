a8d9d358129ab74a2880eba8101316fc8e5711fd
Daniel Brazel <dbrazel4@gmail.com>
2018-03-11 21:46:28 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,15 @@ end
 def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
+
+def using_insert(array, new_el)
+  array.insert(4, new_el)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
